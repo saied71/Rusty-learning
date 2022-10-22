8e933d6067003e85d47ad58ece621ece45e5ee79
Saied Alimoradi <saied@pop-os.localdomain>
2022-10-22 18:34:33 +0330

    review match, structs

--- play_ground/src/main.rs
+++ play_ground/src/main.rs
@@ -1,3 +1,12 @@
+// fn main() {
+//  let p = 100;
+//  match p {
+//     n @ 10 ..= 17 => println!("{}", n),
+//     n @ 1 ..= 5 => println!("sec {}", n),
+//     _ => {}
+//  }
+// }
+
 fn main() {
-    println!("test");
+    
 }
