4855d166de9c07e507ce90d78981097a3372b30f
Saied Alimoradi <saied@pop-os.localdomain>
2022-11-25 17:41:56 +0330

    strating Rust_book

--- play_ground/src/main.rs
+++ play_ground/src/main.rs
@@ -1,34 +1,12 @@
-use std::collections::HashMap;
+
 
 fn main() {
-    ////////// USING HASHMAP with MATCH ///////////////////
-    // let mut hm = HashMap::new();
-
-    // hm.insert(String::from("asshole"), 13);
-    // hm.insert(String::from("asdasdasdasd"), 12);
-
-    // for (k, v) in &hm {
-        
-    //     println!("{}, {}", k, v)
-    // }
-
-    // match hm.get(&String::from("asshole")) {
-    //     Some(&n) => println!("{}", n),
-    //     _ => println!("mo match"),
-    // }
-    //////////////// USING while let instead of match --> exustive
-    // let mut s = Some(0);
     
-    // while let Some(i) = s {
-    //     if i > 20 {
-    //         println!("Quit");
-    //         s = None;
-    //     } else {
-    //         println!("{}", i);
-    //         s = Some(i + 2);
-    //     }
-    // }
+    let x: (i32, f64, u8) = (50, 6.45, 1);
 
+    println!("{:?}", x);
 
-    
+    println!("{}", x.2);
+
+    // until array type: https://doc.rust-lang.org/book/ch03-02-data-types.html
 }
