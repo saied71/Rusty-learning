ecedd9d6b46e6354384eaedbf0c9c6b09dd4c9da
saied71 <saied.alimoradi@gmail.com>
2023-04-09 17:53:14 +0330

    string part of course

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,8 @@
+use std::net::AddrParseError;
+
 fn main() {
-    addr = String::new("127.0.0.1:8080");
-    let server = Server::new("127.0.0.1:8080");
+    let addr = String::from("127.0.0.1:8080".to_string());
+    let server = Server::new(addr);
     server.run();
 }
 
@@ -9,13 +11,13 @@ struct Server {
 }
 
 impl Server {
-    fn new(addr: String) -> Server {
-        Server {
+    fn new(addr: String) -> Self {
+        Self {
             addr
         }
     }
 
     fn run(self) {
-
+        println!("Listening on {}", self.addr)
     }
 }
